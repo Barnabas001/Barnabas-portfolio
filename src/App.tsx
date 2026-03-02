46d32b6efa7ad6f0de24e1d226cc58c9ee826c8a
Barnabas Affonshike <117881010+Barnabas001@users.noreply.github.com>
2026-03-02 11:26:05 +0100

    put placeholder on app file

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import Navbar from "./components/layout/Navbar";
 export default function App() {
   return (
     <main className=" bg-cyber-black min-h-screen">
-      {/* <h1 className="font-display text-cyber-blue text-4xl p-8">
-        Portfolio Loading...
-      </h1> */}
       <Navbar />
+
+      <div className="h-screen flex items-center justify-center">
+        <h1 className="font-display text-cyber-blue text-5xl">Coming Soon</h1>
+      </div>
+      <div className="h-screen flex items-center justify-center">
+        <h1 className="font-display text-cyber-purple text-5xl">
+          About Coming Soon
+        </h1>
+      </div>
     </main>
   );
 }
